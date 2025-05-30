31c96182fa37810f08df4a9ce990b85ee9f0042d
Fajar956 <fajarsetianusa@gmail.com>
2025-05-30 10:38:19 +0700

    menambahkan foto adit

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -83,7 +83,7 @@ function AboutPage() {
                 {
                   name: "Adith Kurniawan",
                   image:
-                    "https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=800&q=80",
+                    "/adit.jpg",
                 },
                 {
                   name: "Muthia Maylafayza",
